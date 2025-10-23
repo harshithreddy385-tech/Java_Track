6ad71bba3ec5fc1541e5d91d2e25d51567e2158d
Harshithreddy <Harshithreddy385@gmail.com>
2025-10-23 20:15:12 +0530

    Addition file added

---
+++ Variable.java
@@ -0,0 +1,19 @@
+ //The data type is of two types
+//1. Primitive data type:It  stores the actual value e.g; int, float, char, boolean
+//2.Reference data type: It stores the address of the value e.g., Arrays, Classes, Interfaces
+//Steps to declare a variable
+//declare a variable and assign a value to it
+public class Variable {
+    public static void main(String[] args) {
+        //declaring variables
+        int age = 10; // integer variable
+        float price = 20.5f; // float variable
+
+
+        //printing variables
+        System.out.println("Integer: " + age);
+        System.out.println("Float: " + price);
+        
+      
+    }
+}  
