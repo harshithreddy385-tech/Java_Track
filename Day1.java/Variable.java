4e455d47c7e52e1c00711ed1a010057c9617902c
Harshithreddy <Harshithreddy385@gmail.com>
2025-10-23 19:52:41 +0530

    Add Variable.java with primitive data types examples

---
+++ Day1.java/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        // Primitive data types example
+        int age = 18;
+        double price = 19.89;
+        char grade = 'A';
+        boolean isStudent = true;
+        
+        // Printing variables
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student? " + isStudent);
+    }
+}
