41eea42ef18e0f746488d6b358ca682df83bc16c
Harshithreddy <Harshithreddy385@gmail.com>
2025-10-23 19:57:37 +0530

    Update Variable.java with primitive data types examples

--- Day1.java/Variable.java
+++ Day1.java/Variable.java
@@ -1,15 +1,19 @@
+ //The data type is of two types
+//1. Primitive data type:It  stores the actual value e.g; int, float, char, boolean
+//2.Reference data type: It stores the address of the value e.g., Arrays, Classes, Interfaces
+//Steps to declare a variable
+//declare a variable and assign a value to it
 public class Variable {
     public static void main(String[] args) {
-        // Primitive data types example
-        int age = 18;
-        double price = 19.89;
-        char grade = 'A';
-        boolean isStudent = true;
+        //declaring variables
+        int age = 10; // integer variable
+        float price = 20.5f; // float variable
+
+
+        //printing variables
+        System.out.println("Integer: " + age);
+        System.out.println("Float: " + price);
         
-        // Printing variables
-        System.out.println("Age: " + age);
-        System.out.println("Price: " + price);
-        System.out.println("Grade: " + grade);
-        System.out.println("Is Student? " + isStudent);
+      
     }
-}
+}  
