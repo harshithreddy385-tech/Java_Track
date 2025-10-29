95895f2cba477283e8572564dfedca5bbc8cc1dc
Harshithreddy <Harshithreddy385@gmail.com>
2025-10-29 16:51:31 +0530

    Added Variable.java with basic Java variable types and printing

--- Variable.java
+++ Variable.java
@@ -1,4 +1,4 @@
- //The data type is of two types
+//Variable: A variable is a container that holds data that can be changed during program execution.
 //1. Primitive data type:It  stores the actual value e.g; int, float, char, boolean
 //2.Reference data type: It stores the address of the value e.g., Arrays, Classes, Interfaces
 //Steps to declare a variable
@@ -8,12 +8,22 @@ public class Variable {
         //declaring variables
         int age = 10; // integer variable
         float price = 20.5f; // float variable
-
+        String number = "Ten"; // String variable
 
         //printing variables
         System.out.println("Integer: " + age);
-        System.out.println("Float: " + price);
+        System.out.println("Float: " + price + " /-");
+        System.out.println("String: " + number + " Chocolates");
+
+        String name = "Harshith";
+        int age1 = 20;
+        double gpa = 8.5;
+
+        System.out.println(name +" of age "+ age1 + " has a GPA of " + gpa);
         
-      
+        
+
+
     }
-}  
+
+}
